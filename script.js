b80d9c153d306d30a0c1b2e7315aa654ef376b4c
Cardanoian <superiorz1@yu.ac.kr>
2025-07-06 22:17:01 +0900

    init

---
+++ script.js
@@ -0,0 +1,11 @@
+const jisuBtn = document.querySelector('#jisu');
+const jennyBtn = document.querySelector('#jenny');
+const roseBtn = document.querySelector('#rose');
+const lisaBtn = document.querySelector('#lisa');
+const blackpinkBtn = document.querySelector('#blackpink');
+
+jisuBtn.addEventListener('click', () => alert('지수 버튼 누름'));
+jennyBtn.addEventListener('click', () => alert('제니 버튼 누름'));
+roseBtn.addEventListener('click', () => alert('로제 버튼 누름'));
+lisaBtn.addEventListener('click', () => alert('리사 버튼 누름'));
+blackpinkBtn.addEventListener('click', () => alert('블랙핑크 버튼 누름'));
